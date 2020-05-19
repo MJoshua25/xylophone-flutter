c77e265d09f199ae7f3a035781ae852408464f4b
Yao Josué <jyao2000@yahoo.fr>
2020-05-20 00:43:27 +0200

    Create buttons

--- lib/main.dart
+++ lib/main.dart
@@ -9,16 +9,76 @@ class XylophoneApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(
-            child: Center(
-              child: FlatButton(
-                onPressed: (){
-                  final player = AudioCache();
-                  player.play('note1.wav');
-                },
-                child: Text('Click me'),
+          child: Column(
+            children: <Widget>[
+              Expanded(
+                child: FlatButton(
+                  color: Colors.red,
+                  onPressed: (){
+                    final player = AudioCache();
+                    player.play('note1.wav');
+                  },
+                  child: Text('Click me'),
+                ),
               ),
-            ),
+              Expanded(
+                child: FlatButton(
+                  color: Colors.orange,
+                  onPressed: (){
+                    final player = AudioCache();
+                    player.play('note2.wav');
+                  },
+                  child: Text('Click me'),
+                ),
+              ),
+              Expanded(
+                child: FlatButton(
+                  color: Colors.yellow,
+                  onPressed: (){
+                    final player = AudioCache();
+                    player.play('note3.wav');
+                  },
+                  child: Text('Click me'),
+                ),
+              ),Expanded(
+                child: FlatButton(
+                  color: Colors.green,
+                  onPressed: (){
+                    final player = AudioCache();
+                    player.play('note4.wav');
+                  },
+                  child: Text('Click me'),
+                ),
+              ),Expanded(
+                child: FlatButton(
+                  color: Colors.teal,
+                  onPressed: (){
+                    final player = AudioCache();
+                    player.play('note5.wav');
+                  },
+                  child: Text('Click me'),
+                ),
+              ),Expanded(
+                child: FlatButton(
+                  color: Colors.blue,
+                  onPressed: (){
+                    final player = AudioCache();
+                    player.play('note6.wav');
+                  },
+                  child: Text('Click me'),
+                ),
+              ),
+              Expanded(
+                child: FlatButton(
+                  color: Colors.purple,
+                  onPressed: (){
+                    final player = AudioCache();
+                    player.play('note7.wav');
+                  },
+                  child: Text('Click me'),
+                ),
+              ),
+            ],
           ),
         ),
       ),
